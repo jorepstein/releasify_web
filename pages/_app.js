5d9e18ea0a1372c39bc7c4a1468f21cc89506a27
Jordan Epstein <jordan.epstein@schrodinger.com>
2021-12-27 20:58:57 -0700

    Add .prettierrc, update next.js, add a lot of components

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import {SessionProvider} from 'next-auth/react';
 import '../styles/globals.css';
+import { createTheme, ThemeProvider  } from '@mui/material/styles';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 export default function App({Component, pageProps: {session, ...pageProps}}) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={darkTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,119 @@
-import { useSession, signIn, signOut } from "next-auth/react"
-import {useState} from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
-export default function Component() {
+export default function AppBox() {
+  const { data: session } = useSession();
 
-  const {data: session} = useSession();
-  const [list, setList] = useState([]);
-
-  const getMyPlaylists = async () => {
-    const res = await fetch('/api/playlists');
-    console.log(res);
-    const f = await res.json();
-    console.log("F!!!!!!!!!!!!!!!!!", f);
-    const {items} = f;
-    setList(items);
-    console.log(items);
-  };
-
-  if(session) {
+  return (
+    <Box
+      sx={{
+        bgcolor: "background.default",
+        color: "text.primary",
+      }}
+    >
+      <LogIn />
+      <hr />
+      <Component />
+    </Box>
+  );
+}
+function LogIn() {
+  const { data: session } = useSession();
+  if (session) {
     return (
       <>
         Signed in as {session?.token?.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-        <hr />
-        <button onClick={() => getMyPlaylists()}>Get all my playlists</button>
-        {list.map((item) => (
-          <div key={item.id}>
-            <h1>{item.name}</h1>
-            <img src={item.images[0]?.url} width="100" />
-          </div>
-        ))}
       </>
     );
   }
-  return <>
-    Not signed in <br/>
-    <button onClick={() => signIn("spotify")}>Sign in</button>
-  </>
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn("spotify")}>Sign in</button>
+    </>
+  );
 }
 
-function PlaylistList() {
-  return <List>
+function Component() {
+  const { data: session } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+  const [selectedPlaylistIds, setSelectedPlaylistIds] = useState(new Set());
 
-  </List>
+  const getMyPlaylists = async () => {
+    const res = await fetch("/api/playlists");
+    const f = await res.json();
+    const { items } = f;
+    setPlaylists(items);
+  };
+
+  const onPlaylistClick = (event, playlistId) => {
+    if( selectedPlaylistIds.has(playlistId)) {
+      selectedPlaylistIds.delete(playlistId);
+    }else {
+      selectedPlaylistIds.add(playlistId);
+    }
+    setSelectedPlaylistIds(new Set(selectedPlaylistIds));
+  };
+
+  if (session) {
+    return (
+      <>
+        <button onClick={() => getMyPlaylists()}>Get all my playlists</button>
+        <PlaylistBox
+          playlists={playlists}
+          selectedPlaylistIds={selectedPlaylistIds}
+          onPlaylistClick={onPlaylistClick}
+        />
+        <RunButton />
+      </>
+    );
+  }
+  return <div />;
+}
+
+function PlaylistBox(props) {
+  return (
+    <Box sx={{ overflow: "auto", height: "100%", maxHeight: "500px" }}>
+      <PlaylistList
+        {...props}
+      />
+    </Box>
+  );
+}
+
+function PlaylistList({ playlists, selectedPlaylistIds, onPlaylistClick }) {
+  return (
+    <List sx={{ height: "100%" }}>
+      {playlists.map((item) => (
+        <Playlist
+          key={item.id}
+          name={item.name}
+          imageUrl={item.images[0]?.url}
+          selected={selectedPlaylistIds.has(item.id)}
+          onPlaylistClick={(event) => {onPlaylistClick(event, item.id)}}
+        />
+      ))}
+    </List>
+  );
+}
+
+function Playlist({ name, imageUrl, selected, onPlaylistClick }) {
+  return (
+    <ListItemButton selected={selected} onClick={onPlaylistClick}>
+      <img src={imageUrl} width="50" />
+      <ListItemText primary={name} />
+    </ListItemButton>
+  );
+}
+
+function RunButton() {
+  return <Button variant="outline">Run</Button>;
 }
